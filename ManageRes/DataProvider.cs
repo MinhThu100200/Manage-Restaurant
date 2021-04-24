4892d367a5c0695121d04e5e132f31e69ca4d7b7
MinhThu100200 <18128062@student.hcmute.edu.vn>
2021-04-24 23:21:10 +0700

    update

---
+++ ManageRes/DataProvider.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageRes
+{
+    class DataProvider
+    {
+        private string connectionDT = "Data Source=DESKTOP-62EBFHC;Initial Catalog=QLNH;Integrated Security=True";
+       
+        private static DataProvider instance;
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return DataProvider.instance;
+            }
+            private set { DataProvider.instance = value; }
+        }
+        public DataProvider()
+        {
+
+        }
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionDT))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] paraList = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in paraList)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                return data;
+            }
+        }
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionDT))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] paraList = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in paraList)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+                connection.Close();
+                return data;
+            }
+        }
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionDT))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] paraList = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in paraList)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+                connection.Close();
+                return data;
+            }
+        }
+    }
+}
