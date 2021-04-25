2650a2bc65bcc742153fcd0039dd33a1acecd855
MinhThu100200 <18128062@student.hcmute.edu.vn>
2021-04-25 23:22:03 +0700

    update

---
+++ ManageRes/UserInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageRes
+{
+    class UserInfo
+    {
+        private int Id;
+        private int userId;
+        private string firstName;
+        private string lastName;
+        private DateTime birthDay;
+        private string address;
+        private Image image;
+        private string phone;
+       
+        public UserInfo()
+        { }
+
+        public UserInfo(DataRow row)
+        {
+            this.Id = (int)row["ID"];
+            this.UserId = (int)row["ID User"];            
+            this.FirstName = row["FirstName"].ToString();
+            this.LastName = row["LastName"].ToString();
+            this.birthDay = (DateTime)row["BirthDay"];
+            this.Address = row["Address"].ToString();           
+            if (row["Image"].ToString() != "")
+            {
+                Byte[] pic = (byte[])row["Image"];
+                MemoryStream picture = new MemoryStream(pic);
+                this.image = Image.FromStream(picture);
+            }
+            this.Phone = row["Phone"].ToString();
+
+        }
+
+        public int ID { get => Id; set => Id = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public DateTime BirthDay { get => birthDay; set => birthDay = value; }
+        public string Address { get => address; set => address = value; }        
+        public Image Image { get => image; set => image = value; }
+        public string Phone { get => phone; set => phone = value; }
+    }
+}
---
+++ ManageRes/Users.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageRes
+{
+    class Users
+    {
+        private int Id;
+        private string Username;
+        private string Password;
+        private string email;
+        private int role;
+
+        public Users()
+        { }
+
+        public Users(DataRow row)
+        {
+            this.Id = (int)row["ID"];
+            this.Username = row["Username"].ToString();
+            this.Password = row["Password"].ToString();
+            this.email = row["Email"].ToString();           
+            this.role = (int)row["Role"];
+           
+        }
+        public int ID { get => ID; set => ID = value; }
+        public string UserName { get => Username; set => Username = value; }
+        public string PassWord { get => Password; set => Password = value; }       
+        public string Email { get => email; set => email = value; }
+        public int Role { get => role; set => role = value; }
+    }
+}
